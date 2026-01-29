fc6d9a7de5cd1b2dd5f3ff848dcee8351bf4bb4a
Mohamed Cherif Diallo <diallo.mohamed-cherif@ugb.edu.sn>
2026-01-29 00:13:34 +0000

    init

---
+++ src/main/java/sn/test/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package sn.test.calculator;
+
+
+public class Calculator {
+
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public int subtractNumbers(int a, int b) {
+        return a - b;
+    }
+}
